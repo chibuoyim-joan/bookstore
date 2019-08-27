bfe4aba25ab554d446975229d46ab6b7cff48522
Chibuoyim joan <orjiakojoan@gmail.com>
2019-08-26 17:17:22 -0700

    created a book class

---
+++ bookclass.java
@@ -0,0 +1,32 @@
+public class Book{
+	private String title;
+	private String category;
+	private int pages;
+	private String author 
+
+
+public void settitlename(String book){
+	this.title = title;
+}
+public void setcategoryname(String category){
+	this.category = category;
+}
+public void setpagesname(int pages){
+	this.pages = pages;
+}
+public void setauthorname (String author){
+	this.author = author;
+}
+public String gettitlename(){
+	return this.title;
+}
+public String getauthorname(){
+	return this.author;
+}
+public int getpagename(){
+	return this.pages;
+}
+public String getcategory(){
+	return this, category;
+}
+}
